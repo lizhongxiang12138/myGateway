37f096631b59ac57f162d6c9f1c79c4fbd057652
lzx <953934680@qq.com>
2019-07-02 14:18:02 +0800

    注释的完善

--- src/main/java/com/lzx/gateway/demo/MyGatewayApplication.java
+++ src/main/java/com/lzx/gateway/demo/MyGatewayApplication.java
@@ -2,12 +2,14 @@ package com.lzx.gateway.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @SpringBootApplication springboot启动类注解
+ * @EnableEurekaClient 启用eureka客服端
+ */
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
 public class MyGatewayApplication {
 
 	public static void main(String[] args) {
